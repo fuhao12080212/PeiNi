f37f8460ec8c0aa6aaaf6d91b8239f12024ebfdc
付豪 <fuhao04@meituan.com>
2016-03-08 14:16:13 +0800

    no message

---
+++ PeiNi/app/src/main/java/com/peini/ui/activity/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.peini.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fuhao on 16/3/7.
+ */
+public class RegisterActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ PeiNi/app/src/main/java/com/peini/ui/fragment/UserFragment.java
@@ -0,0 +1,7 @@
+package com.peini.ui.fragment;
+
+/**
+ * Created by fuhao on 16/3/7.
+ */
+public class UserFragment {
+}
